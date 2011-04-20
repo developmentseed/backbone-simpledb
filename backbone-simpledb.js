1944287265f09bc3f2766b7881bd247ca88ddda6
Young Hahn <young@developmentseed.org>
2011-04-20 14:59:11 -0400

    Handle NextToken requests for Collections.

--- backbone-simpledb.js
+++ backbone-simpledb.js
@@ -32,6 +32,8 @@ module.exports = function(options) {
             return object.url;
         }
     };
+
+    // Pack/unpack objects into individually JSON stringified attributes.
     var unpack = function(data) {
         for (var key in data) {
             if (key === '$ItemName') {
@@ -49,6 +51,28 @@ module.exports = function(options) {
         return data;
     };
 
+    // Wrapper around sdb.select. Retrieves all results for a select statement,
+    // making subsequent requests if a `NextToken` is returned by SimpleDB.
+    // Note that at some points the result set on the initial query will
+    // actually be null and only a `NextToken` will be provided.
+    var select = function(q, data, next, callback) {
+        var options = {};
+        next && (options.next = next);
+        sdb.select(q, options, function(err, result, meta) {
+            result = result || [];
+            data = data.concat(result);
+            if (
+                meta.result &&
+                meta.result.SelectResult &&
+                meta.result.SelectResult.NextToken
+            ) {
+                select(q, data, meta.result.SelectResult.NextToken, callback);
+            } else {
+                callback(err, data);
+            }
+        });
+    };
+
     return {
         sdb: sdb,
         sync: function(method, model, success, error) {
@@ -64,14 +88,11 @@ module.exports = function(options) {
                     );
                 } else {
                     var data = [],
-                        base = getUrl(model);
-                    sdb.select(
-                        'SELECT * FROM ' + options.domain + ' WHERE ItemName() LIKE "' + base + '%"',
-                        function(err, data) {
-                            data = data || [];
-                            success(_(data).map(unpack));
-                        }
-                    );
+                        base = getUrl(model),
+                        q = 'SELECT * FROM ' + options.domain + ' WHERE ItemName() LIKE "' + base + '%"';
+                    select(q, data, null, function(err, data) {
+                        success(_(data).map(unpack));
+                    });
                 }
                 break;
             case 'create':
